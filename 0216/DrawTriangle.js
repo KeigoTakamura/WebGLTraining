94f5c7bc2cf11f68c1490cfdef244bab093315e5
takamura.keigo.se@gmail.com <takamura.keigo.se@gmail.com>
2021-02-16 20:52:09 +0900

    fix:三角形

---
+++ 0216/DrawTriangle.js
@@ -0,0 +1,24 @@
+//三角形を書きたい
+
+function main(){
+    var canvas = document.getElementById('example');
+
+    if(!canvas){
+        console.log('Canvas要素の取得に失敗');
+        return;
+    }
+
+    var ctx = canvas.getContext('2d');
+
+    //三角形を書く
+
+    ctx.beginPath();
+    ctx.moveTo(120,10);
+    ctx.lineTo(200,150);
+    ctx.lineTo(40,150);
+    
+    ctx.closePath();
+
+    ctx.fillStyle = 'rgba(0,0,255,1.0)';
+    ctx.fill();
+}
