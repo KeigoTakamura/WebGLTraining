58ddd18f6e22e6926eb575441da3f14e43f6aa2f
takamura.keigo.se@gmail.com <takamura.keigo.se@gmail.com>
2021-02-19 20:56:07 +0900

    update

--- 0217/HelloCanvas.js
+++ 0217/HelloCanvas.js
@@ -2,7 +2,7 @@
 
 function main(){
     var VSHAADER_SOURCE = 
-    ' void main() {\n ' +
+    'void main() {\n ' +
     'gl_Position =  vec4(0.0,0.0,0.0,1.0); \n' +
     'gl_PointSize = 10.0;\n' +
     '}\n';
@@ -12,18 +12,25 @@ function main(){
         'gl_FragColor = vec4(0.0,0.0,0.0,1.0); \n' +
         '}\n';
 
-
     var canvas = document.getElementById('webgl');
-    var gl = canvas.getContext("webgl2") ||
-      canvas.getContext("webgl") ||
-      canvas.getContext("experimental-webgl");
+    var gl = getWebGLContext(canvas);
     if(!gl){
         console.log('WebGLコンテキストが取れなかった');
         return;
     }
-    
+    if(!initShaders(gl,VSHAADER_SOURCE,FSHA_SOURCE)){
+        console.log('Shaderのrefresh');
+        return;
+    }    
     gl.clearColor(1.0,0, 0.0, 0.9);
-
     //Canvas
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
+
+function getWebGLContext(opt_canvas) {
+    var canvas = opt_canvas || document.createElement('canvas');
+    
+    return canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  }
